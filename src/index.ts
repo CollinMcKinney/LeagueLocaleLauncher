db6532e33b54d2d9b77c34dcb591a8f3f5b03fff
CollinMcKinney <collinmckinney4@gmail.com>
2022-10-07 23:08:47 -0500

    Small cleanup.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ function createWindows (): void {
     mainWindow = new BrowserWindow({
         width: 900, height: 600,
         webPreferences: {
+            // WARNING: Not secure if loading external websites. Though this `fixes`
+            // some errors with imports; maybe there is a better workaround.
+            nodeIntegration: true,
             preload: __dirname + "/preload.js"
         },
         show: false
